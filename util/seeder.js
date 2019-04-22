3fd68de3317bbbc4f9e3d55529558f77b966a064
Chris Broveleit <Chris.Broveleit@gmail.com>
2019-04-22 10:38:39 -0700

    seeding complete

--- util/seeder.js
+++ util/seeder.js
@@ -6,7 +6,7 @@ const csvWriter = require('csv-write-stream');
 const writer = csvWriter();
 const fs = require('fs');
 
-writer.pipe(fs.createWriteStream('photos.csv'))
+writer.pipe(fs.createWriteStream('photos10.csv'))
 
 const createProperties = () => {
   let i = 0;
@@ -20,7 +20,7 @@ const createProperties = () => {
      }),
      bed_count: faker.random.number({
        min: 2,
-       max: 30,
+       max: 8,
      }),
      bath_count: faker.random.number({
        min: 1,
@@ -29,9 +29,7 @@ const createProperties = () => {
      sq_ft: faker.random.number({
        min: 1000,
        max: 30000,
-     }),
-     createdAt: faker.date.past(.3),
-     updatedAt: faker.date.past(.3),
+     })
    })
  }
   const helper = () => {
@@ -44,7 +42,7 @@ const createProperties = () => {
         ready2Print = writer.write(createNewProperty());
       }
     } while(i < 10000000 && ready2Print);
-    if(i < 1000000){
+    if(i < 10000000){
       writer.once('drain', helper);
     }
   }
@@ -60,8 +58,6 @@ const createPhotos = () => {
       { id: i,
         url: `https://s3-us-west-2.amazonaws.com/sdcphotosbroveleit/large${faker.random.number( {min: 1, max:999 })}.jpg`,
         property_id: listing,
-        createdAt: faker.date.past(.3),
-        updatedAt: faker.date.past(.3),
       }
     )
   }
@@ -78,13 +74,13 @@ const createPhotos = () => {
         listing++
         randomPhotoCount = faker.random.number({min: 10, max:15});
       }
-      if(listing === 10000001){
+      if(listing === 10000002){
         writer.end();
       } else {
         ready2Print = writer.write(createPhoto());
       }
-      } while(listing <= 10000001 && ready2Print);
-      if(listing <= 10000001){
+      } while(listing <= 10000002 && ready2Print);
+      if(listing <= 10000002){
         writer.once('drain', helper);
       }
   }
