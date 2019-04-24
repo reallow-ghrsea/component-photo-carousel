aa1b345d584f9ab8c19a00f741402e90cde9bb92
Chris Broveleit <Chris.Broveleit@gmail.com>
2019-04-24 16:26:52 -0700

    seed algorithm changed to create only one table with an array of photos for last column

--- util/seeder.js
+++ util/seeder.js
@@ -11,6 +11,11 @@ writer.pipe(fs.createWriteStream('photos10.csv'))
 const createProperties = () => {
   let i = 0;
   const createNewProperty = () => {
+    var randomPhotoCount = faker.random.number({min: 10, max:15});
+    var photoArray = [];
+    for(var i = 0; i < randomPhotoCount; i++){
+      photoArray.push(`https://s3-us-west-2.amazonaws.com/sdcphotosbroveleit/large${faker.random.number( {min: 1, max:999 })}.jpg`)
+    }
     return ({
      id: i,
      name: faker.address.streetAddress(),
@@ -29,7 +34,8 @@ const createProperties = () => {
      sq_ft: faker.random.number({
        min: 1000,
        max: 30000,
-     })
+     }),
+     photos: photoArray 
    })
  }
   const helper = () => {
@@ -50,42 +56,41 @@ const createProperties = () => {
   helper();
 }
 
-const createPhotos = () => {
-  var i = 1;
-  var listing = 1;
-  const createPhoto = () => {
-    return(
-      { id: i,
-        url: `https://s3-us-west-2.amazonaws.com/sdcphotosbroveleit/large${faker.random.number( {min: 1, max:999 })}.jpg`,
-        property_id: listing,
-      }
-    )
-  }
+// const createPhotos = () => {
+//   var i = 1;
+//   var listing = 1;
+//   const createPhoto = () => {
+//     return(
+//       { id: i,
+//         url: `https://s3-us-west-2.amazonaws.com/sdcphotosbroveleit/large${faker.random.number( {min: 1, max:999 })}.jpg`,
+//         property_id: listing,
+//       }
+//     )
+//   }
 
-  const helper = () => {
-    let ready2Print = true;
-    var randomPhotoCount = faker.random.number({min: 10, max:15});
-    var counter = 0;
-    do{
-      i++;
-      counter++;
-      if(counter > randomPhotoCount){
-        counter = 0;
-        listing++
-        randomPhotoCount = faker.random.number({min: 10, max:15});
-      }
-      if(listing === 10000002){
-        writer.end();
-      } else {
-        ready2Print = writer.write(createPhoto());
-      }
-      } while(listing <= 10000002 && ready2Print);
-      if(listing <= 10000002){
-        writer.once('drain', helper);
-      }
-  }
+//   const helper = () => {
+//     let ready2Print = true;
+//     var randomPhotoCount = faker.random.number({min: 10, max:15});
+//     var counter = 0;
+//     do{
+//       i++;
+//       counter++;
+//       if(counter > randomPhotoCount){
+//         counter = 0;
+//         listing++
+//         randomPhotoCount = faker.random.number({min: 10, max:15});
+//       }
+//       if(listing === 10000002){
+//         writer.end();
+//       } else {
+//         ready2Print = writer.write(createPhoto());
+//       }
+//       } while(listing <= 10000002 && ready2Print);
+//       if(listing <= 10000002){
+//         writer.once('drain', helper);
+//       }
+//   }
 
-  helper();
-}
+//   helper();
+// }
 
-createPhotos();
