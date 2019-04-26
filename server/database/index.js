54e507e2973272f896714246b1b541c11eec013d
Chris Broveleit <Chris.Broveleit@gmail.com>
2019-04-26 15:56:12 -0700

    postgres connected

--- server/database/index.js
+++ server/database/index.js
@@ -1,21 +1,36 @@
 const { Client } = require('pg')
 const client = new Client({
-  host: localhost,
-  port: 3333,
-  database: photos
+  database: 'photos'
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
 })
 
-client.connect()
+const getDetails = id => {
+  client.query(`SELECT * FROM properties WHERE id = ${id}`, (err, res) => {
+    if(err) {
+      res.status(500);
+      console.log(err)
+    } else{
+      res.send()
+    }
+  })
+}
 
-const getDetails = id => Property.findAll({
-  where: { id },
-});
-
-const getPhotos = id => Photo.findAll({
-  attributes: ['url'],
-  where: {
-    property_id: id,
-  },
-});
+const getPhotos = id => {
+  client.query(`SELECT * FROM listing_photos WHERE property_id = ${id}`, (err, res) => {
+    if(err){
+      res.status(500);
+      console.log(err);
+    } else {
+      res.send();
+    }
+  })
+}
 
 module.exports = { getDetails, getPhotos };
